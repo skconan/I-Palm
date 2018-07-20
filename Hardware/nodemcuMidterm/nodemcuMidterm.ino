dc63b9065b21443982b9c01c398e105ba5e63622
skconan <supakit.kr@gmail.com>
2018-07-20 15:57:11 +0700

    finish nodeMCU

--- Hardware/nodemcuMidterm/nodemcuMidterm.ino
+++ Hardware/nodemcuMidterm/nodemcuMidterm.ino
@@ -5,15 +5,18 @@
 
 SoftwareSerial se_read(D5, D6); // write only
 SoftwareSerial se_write(D0, D1); // read only
+//String const url = "http://exceed.srakrn.me/api/";
 String const url = "http://ecourse.cpe.ku.ac.th:1515/api/";
 
 struct ProjectData {
   int32_t sw;
-  int32_t buzzer;
-  int32_t led_Y;
-  int32_t led_B;
-  int32_t led_W;
-} project_data = { 0, 0, 0 , 0, 0};
+  int32_t led_Y; // air
+  int32_t led_B; //lightOut
+  int32_t led_W; //lightIn
+  float humid;
+  int32_t light; //ldr
+  
+} project_data = { 0, 0, 0 , 0};
 
 struct ServerData {
   int32_t swPressed;
@@ -21,8 +24,7 @@ struct ServerData {
   int32_t lightIn;
   int32_t lightOut;
   int32_t doorOn;
-  int32_t ringOn;
-} server_data = { 0, 0, 0, 0, 0, 0};
+} server_data = { -1, -1, -1, -1, -1};
 
 const char GET_SERVER_DATA = 1;
 const char GET_SERVER_DATA_RESULT = 2;
@@ -165,8 +167,8 @@ void get_request(String const &str, float &value){
 void get_request_raw_callback(String const &str) {
   Serial.println("GET REQUEST RESPONSE BEGIN");
   Serial.println("========================");
-  Serial.println(str.c_str());
-  Serial.println("========================");
+//  Serial.println(str.c_str());
+//  Serial.println("========================");
   Serial.println("GET REQUEST RESPONSE END");
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -179,7 +181,7 @@ void setup() {
   Serial.print("ESP READY!");
 }
 void print_get_server(String const &str,int32_t a, int32_t b){
-  Serial.print(str);
+  Serial.print(">>>> " + str);
   if (a == b)
     Serial.println(": On");
   else
@@ -194,6 +196,7 @@ void loop() {
   uint32_t cur_time = millis();
   if (cur_time - last_sent_time > 1000) {
     //int variable
+
     GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-switchStatus/view/", get_request,server_data.swPressed);
     GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-doorOn/view/", get_request,server_data.doorOn);
     GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-airOn/view/", get_request,server_data.airOn);
@@ -231,23 +234,37 @@ void loop() {
           case UPDATE_PROJECT_DATA: {
               ProjectData *project_data = (ProjectData*)buffer;
               Serial.println("<---- From arduino ---- >");
-         
-              int32_t switchStatus = project_data->sw;         
-              Serial.println(switchStatus);  
-              POST(set_builder("iPalm-switchStatus", switchStatus).c_str(), update_data_to_server_callback);
-              POST(set_builder("iPalm-switchStatus", switchStatus).c_str(), update_data_to_server_callback);
+//              int32_t sw;
+//              int32_t led_Y; // air
+//              int32_t led_B; //lightOut
+//              int32_t led_W; //lightIn
+//              float humid;
+//              int32_t light; //ldr
+              print_get_server("iPalm-switchStatus", project_data->sw, 0);
+              POST(set_builder("iPalm-switchStatus", project_data->sw).c_str(), update_data_to_server_callback);
+              
+              print_get_server("iPalm-airOn", project_data->led_Y, 1);
+              POST(set_builder("iPalm-airOn", project_data->led_Y).c_str(), update_data_to_server_callback);
+              
+              print_get_server("iPalm-lightOut", project_data->led_B, 1);
+              POST(set_builder("iPalm-lightOut", project_data->led_B).c_str(), update_data_to_server_callback);             
+              
+              print_get_server("iPalm-lightIn", project_data->led_W, 1);
+              POST(set_builder("iPalm-lightIn", project_data->led_W).c_str(), update_data_to_server_callback);
+
+              Serial.print("iPalm-humid: ");
+              Serial.println(project_data->humid);
+              POST(set_builder("iPalm-humid", project_data->humid).c_str(), update_data_to_server_callback);             
 
             }
             break;
           case GET_SERVER_DATA:
-
             Serial.println("<---- Send data to arduino ---->");
             print_get_server("switchStatus", server_data.swPressed, 0);
             print_get_server("doorOn", server_data.doorOn, 1);
             print_get_server("airOn", server_data.airOn, 1);
             print_get_server("lightOut", server_data.lightOut, 1);
             print_get_server("lightIn", server_data.lightIn, 1);
-            
             send_to_arduino(GET_SERVER_DATA_RESULT, &server_data, sizeof(ServerData));
             break;
         }
