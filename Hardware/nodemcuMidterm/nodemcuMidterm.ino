3ea94a17d179b420d18ba2c8bc4798d0c981915a
skconan <supakit.kr@gmail.com>
2018-07-20 14:18:18 +0700

    finished Get data from server

--- Hardware/nodemcuMidterm/nodemcuMidterm.ino
+++ Hardware/nodemcuMidterm/nodemcuMidterm.ino
@@ -5,34 +5,38 @@
 
 SoftwareSerial se_read(D5, D6); // write only
 SoftwareSerial se_write(D0, D1); // read only
-String const url = "http://ecourse.cpe.ku.ac.th:1515/";
+String const url = "http://ecourse.cpe.ku.ac.th:1515/api/";
 
 struct ProjectData {
-  int32_t is_button_pressed;
-  float temp;
-  int32_t light_lux;
-  float sound;
-  int32_t door;
-  int32_t plus;
-} cur_project_data;
+  int32_t sw;
+  int32_t buzzer;
+  int32_t led_Y;
+  int32_t led_B;
+  int32_t led_W;
+} project_data = { 0, 0, 0 , 0, 0};
 
 struct ServerData {
-  int32_t is_button_pressed;
-  float temp;
-  int32_t light_lux;
-  float sound;
-  int32_t door;
-  int32_t plus;
-} server_data;
+  int32_t swPressed;
+  int32_t airOn;
+  int32_t lightIn;
+  int32_t lightOut;
+  int32_t doorOn;
+  int32_t ringOn;
+} server_data = { 0, 0, 0, 0, 0, 0};
 
 const char GET_SERVER_DATA = 1;
 const char GET_SERVER_DATA_RESULT = 2;
 const char UPDATE_PROJECT_DATA = 3;
 
+const char GET_SERVER_DATA_ACTION = 4;
+const char GET_SERVER_DATA_RESULT_ACTION = 5;
+const char UPDATE_PROJECT_DATA_ACTION = 6;
+
 // wifi configuration
+//const char SSID[] = "Unknow";
+//const char PASSWORD[] = "1q2w3e4r";
 const char SSID[] = "KUWIN_QUEEN_2.4GHz";
 const char PASSWORD[] = "1234567890";
-
 // for nodemcu communication
 uint32_t last_sent_time = 0;
 char expected_data_size = 0;
@@ -105,6 +109,7 @@ bool GET(const char *url, void (*callback)(String const &str,int32_t &value), in
     if (callback != 0) {
       callback(main_client.getString(),value);
     }
+    delay(100);
     return true;
   }
   Serial.println("GET REQUEST RESPONSE BEGIN");
@@ -118,9 +123,11 @@ bool GET(const char *url, void (*callback)(String const &str,float &value), floa
     if (callback != 0) {
       callback(main_client.getString(),value);
     }
+    delay(100);
     return true;
   }
   Serial.println("GET REQUEST RESPONSE BEGIN");
+  
   return false;
 }
 
@@ -132,6 +139,7 @@ bool POST(const char *url, void (*callback)(String const &str)) {
     if (callback != 0) {
       callback(main_client.getString());
     }
+    delay(100);
     return true;
   }
   Serial.println("POST REQUEST RESPONSE BEGIN");
@@ -170,32 +178,40 @@ void setup() {
   Serial.println((int)sizeof(ServerData));
   Serial.print("ESP READY!");
 }
+void print_get_server(String const &str,int32_t a, int32_t b){
+  Serial.print(str);
+  if (a == b)
+    Serial.println(": On");
+  else
+    Serial.println(": Off");
+} 
+
 void loop() {
   /* 1) Server polling data from server every 1500 ms
      2) Arduino always get local data
   */
-
+  Serial.println("LOOP");
   uint32_t cur_time = millis();
   if (cur_time - last_sent_time > 1000) {
     //int variable
-    //GET("http://ku-exceed-backend.appspot.com/api/exceed-door/view/", get_request,server_data.door);
-    //Serial.print("door : ");
-    //Serial.println(server_data.door);
-    //float variable
-    //GET("http://ku-exceed-backend.appspot.com/api/exceed-temperature/view/", get_request,server_data.temp); 
-    //Serial.print("temp : ");
-    //Serial.println(server_data.temp);
-    GET(get_builder("ipalm-exceed-plus").c_str(), get_request,server_data.plus); 
-    Serial.print("plus : ");
-    Serial.println(server_data.plus);
-    //GET("http://ku-exceed-backend.appspot.com/api/exceed_value/set/?value=test", 0);
+    GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-switchStatus/view/", get_request,server_data.swPressed);
+    GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-doorOn/view/", get_request,server_data.doorOn);
+    GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-airOn/view/", get_request,server_data.airOn);
+    GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-lightOut/view/", get_request,server_data.lightOut);
+    GET("http://ecourse.cpe.ku.ac.th:1515/api/ipalm-lightIn/view/", get_request,server_data.lightIn);
+    Serial.println("<---- From Server ---->");
+    
+    print_get_server("switchStatus", server_data.swPressed, 0);
+    print_get_server("doorOn", server_data.doorOn, 1);
+    print_get_server("airOn", server_data.airOn, 1);
+    print_get_server("lightOut", server_data.lightOut, 1);
+    print_get_server("lightIn", server_data.lightIn, 1);
+    
     last_sent_time = cur_time;
   }
 
   while (se_read.available()) {
     char ch = se_read.read();
-    //Serial.print("RECV: ");0
-    //Serial.println((byte)ch);
     if (cur_buffer_length == -1) {
       cur_data_header = ch;
       switch (cur_data_header) {
@@ -214,23 +230,24 @@ void loop() {
         switch (cur_data_header) {
           case UPDATE_PROJECT_DATA: {
               ProjectData *project_data = (ProjectData*)buffer;
-              float temp = project_data->temp;
-              int32_t light_lux = project_data->light_lux;
-              int32_t is_button_pressed = project_data->is_button_pressed;
-              float sound = project_data->sound;
-              int32_t door = project_data->door;
-              int32_t plus = project_data->plus;
-              Serial.println(plus);
-              POST(set_builder("ipalm-exceed-temp", temp).c_str(), update_data_to_server_callback);
-              POST(set_builder("ipalm-exceed-light_lux", light_lux).c_str(), update_data_to_server_callback);
-              POST(set_builder("ipalm-exceed-is_button_pressed", is_button_pressed).c_str(), update_data_to_server_callback);
-              POST(set_builder("ipalm-exceed-sound", sound).c_str(), update_data_to_server_callback);
-              POST(set_builder("ipalm-exceed-door", door).c_str(), update_data_to_server_callback);
-              POST(set_builder("ipalm-exceed-plus", plus).c_str(), update_data_to_server_callback);
+              Serial.println("<---- From arduino ---- >");
+         
+              int32_t switchStatus = project_data->sw;         
+              Serial.println(switchStatus);  
+              POST(set_builder("iPalm-switchStatus", switchStatus).c_str(), update_data_to_server_callback);
+              POST(set_builder("iPalm-switchStatus", switchStatus).c_str(), update_data_to_server_callback);
+
             }
             break;
           case GET_SERVER_DATA:
-            Serial.println("Send data to arduino");
+
+            Serial.println("<---- Send data to arduino ---->");
+            print_get_server("switchStatus", server_data.swPressed, 0);
+            print_get_server("doorOn", server_data.doorOn, 1);
+            print_get_server("airOn", server_data.airOn, 1);
+            print_get_server("lightOut", server_data.lightOut, 1);
+            print_get_server("lightIn", server_data.lightIn, 1);
+            
             send_to_arduino(GET_SERVER_DATA_RESULT, &server_data, sizeof(ServerData));
             break;
         }
@@ -238,4 +255,5 @@ void loop() {
       }
     }
   }
+  delay(500);
 }
