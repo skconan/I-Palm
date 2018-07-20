4d173b8ca5efdacf9496951f29cdfcc48c8284f3
thtiiz <napon-na123@hotmail.com>
2018-07-20 14:27:01 +0700

    auto sensor

--- Hardware/arduinoMidterm/arduinoMidterm.ino
+++ Hardware/arduinoMidterm/arduinoMidterm.ino
@@ -1,21 +1,32 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
+#include "DHT.h"
 #define SW 2
 #define BUZZER 3
 #define LED_Y 4
 #define LED_B 5
 #define LED_W 6
-#define SERVO 7
+#define servo 7
+#define DHTPIN A1
+#define DHTTYPE DHT22
+#define ldr A0
+
 Servo myservo;
 SoftwareSerial se_read(12, 13); // write only
 SoftwareSerial se_write(10, 11); // read only
-
+///////////////////////////////
+char notes[] = "ccggaag ffeeddc ggffeed ggffeed ccggaag ffeeddc "; // a space represents a rest
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 4 };
+int tempo = 300;
+///////////////////////////////
 struct ProjectData {
   int32_t sw;
-  int32_t buzzer;
   int32_t led_Y;
   int32_t led_B;
   int32_t led_W;
-} project_data = { 0, 0, 0 , 0, 0, 0};
+  float humid;
+  int32_t light;
+} project_data = { 0, 0, 0 , 0, 0, 400.0};
 
 struct ServerData {
   int32_t swPressed;
@@ -23,12 +34,29 @@ struct ServerData {
   int32_t lightIn;
   int32_t lightOut;
   int32_t doorOn;
-  int32_t ringOn;
 } server_data = { 0, 0, 0, 0, 0, 0};
 
 const char GET_SERVER_DATA = 1;
 const char GET_SERVER_DATA_RESULT = 2;
 const char UPDATE_PROJECT_DATA = 3;
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void playSong(){
+  for(int i = 0; i < 14; i++) {
+    tone(BUZZER, );
+  }
+}
 
 void send_to_nodemcu(char code, void *data, char data_size) {
   char *b = (char*)data; 
@@ -42,6 +70,8 @@ void send_to_nodemcu(char code, void *data, char data_size) {
   }
 }
 
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(SW, INPUT);
@@ -49,8 +79,10 @@ void setup() {
   pinMode(LED_Y, OUTPUT);
   pinMode(LED_B, OUTPUT);
   pinMode(LED_W, OUTPUT);
-  myservo.attach(SERVO);
+  pinMode(ldr, INPUT);
+  myservo.attach(servo);
   myservo.write(0);
+  dht.begin();
   
   Serial.begin(115200);
   se_read.begin(38400);
@@ -72,16 +104,23 @@ char buffer[256];
 int8_t cur_buffer_length = -1;
 int32_t b = -1;
 void loop() {
+  delay(500);
   uint32_t cur_time = millis();
-  
   //read from sensor....
   int a = digitalRead(SW);
+  project_data.humid = dht.readHumidity();
   project_data.sw = a; //set sw to project data
+  project_data.light = analogRead(ldr);
+  Serial.print("light :");
+  Serial.print(project_data.light);
+  Serial.print(" Humid :");
+  Serial.println(project_data.humid);
+//  project
   if(a == 0){
     Serial.println("Press");
     delay(1000);
   }
-  
+  //Serial.println(server_data.airOn);
   /*project_data.plus = b;
   if(server_data.plus == 1){
     digitalWrite(LED,HIGH);
@@ -119,18 +158,28 @@ void loop() {
           case GET_SERVER_DATA_RESULT: {
             ServerData *data = (ServerData*)buffer;
             //use data to control sensor
-            if(data -> swPressed == 1) {
+            Serial.print("SW: ");
+            Serial.println(data->swPressed);
+            //Serial.println(server_data.swPressed);
+            if(data -> swPressed == 0) {
               tone(BUZZER, 2000);
-              delay(1000);
+              delay(500);
               noTone(BUZZER);
-              Serial.print("Buzzer: ON");
+              Serial.print("Buzzer: ON ");
+              
+            }
+            else if(data -> swPressed == 1) {
+              Serial.print("Buzzer: OFF ");
+            }
+            if(data -> doorOn == 0) {
+              myservo.write(180);
+              //delay(500);
+              //project_data.servo = 1;
             }
             
-            if(data -> swPressed == 1 and data -> doorOn == 0) {
-              myservo.write(180);
-              server_data.lastservo = 1;
-            }
-            else if(data -> swPressed == 1 and data -> doorOn == 1) {
+            else if(data -> doorOn == 1) {
+              myservo.write(0);
+              //project_data.servo = 0;
               
             }
               /*tone(buzzer, 2000);
@@ -139,33 +188,44 @@ void loop() {
               tone(buzzer, 2000);
               delay(200);
               noTone(buzzer);*/
-            if(data -> airOn == 1) {
+            if(data -> airOn == 1 or project_data.humid > 500) {
               digitalWrite(LED_Y, HIGH);
-              Serial.print("Air: ON");
+              Serial.print("Air: ON ");
+              project_data.led_Y = 1;
+              //delay(500);
             }
-            else if(data -> airOn == 0) {
+            else if(data -> airOn == 0 or project_data.humid <= 500) {
               digitalWrite(LED_Y, LOW);
-              Serial.print("Air: OFF");
+              Serial.print("Air: OFF ");
+              project_data.led_Y = 0;
+              //delay(500);
             }
             
-            if(data -> lightOut == 1) {
+            if(data -> lightOut == 1 or project_data.light < 500) {
               digitalWrite(LED_B, HIGH);
-              Serial.print("LightOut: ON");
+              Serial.print("LightOut: ON ");
+              project_data.led_B = 1;
+              //delay(500);
             }
-            else if(data -> lightOut == 0) {
+            else if(data -> lightOut == 0 or project_data.light >= 500) {
               digitalWrite(LED_B, LOW);
-              Serial.print("LightOut: OFF");
+              Serial.print("LightOut: OFF ");
+              project_data.led_B = 0;
+              //delay(500);
             }
 
             if(data -> lightIn == 1) {
               digitalWrite(LED_W, HIGH);
-              Serial.print("LightIn: ON");
+              Serial.print("LightIn: ON ");
+              project_data.led_W = 1;
+              //delay(500);
             }
             else if(data -> lightIn == 0) {
               digitalWrite(LED_W, LOW);
-              Serial.print("LightIn: OFF");
+              Serial.println("LightIn: OFF ");
+              project_data.led_W = 0;
+              //delay(500);
             }
-
             
             
             /*Serial.print("temp status: ");
@@ -186,7 +246,7 @@ void loop() {
             //server_data.plus = data->plus;
             
             send_to_nodemcu(UPDATE_PROJECT_DATA, &project_data, sizeof(ProjectData));
-            Serial.println("Send data");
+            Serial.println("UPDATE");
           } break;
         }
         cur_buffer_length = -1;
